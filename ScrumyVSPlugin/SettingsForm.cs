3404ca74f9466415b6a1f2a6b817f51d3d80fadb
Peter Wibeck <pwibeck@users.noreply.github.com>
2013-01-06 18:52:37 +0100

    Added support for updating font

--- ScrumyVSPlugin/SettingsForm.cs
+++ ScrumyVSPlugin/SettingsForm.cs
@@ -25,65 +25,78 @@ namespace PeterWibeck.ScrumyVSPlugin
             TabControl.TabPages.Clear();
             foreach (WorkItemPrintData printWorkItem in settings.PrintWorkItems)
             {
-                var tab = new TabPage();
-                tab.Name = printWorkItem.Type;
-                tab.Text = printWorkItem.Type;
+                var tab = new TabPage {Name = printWorkItem.Type, Text = printWorkItem.Type};
 
-                var backgroundColor = new Label();
-                backgroundColor.Name = "BGColor";
-                backgroundColor.Text = "Background Color";
-                backgroundColor.Location = new Point(6, 15);
-                backgroundColor.Size = new Size(92, 13);
+                var backgroundColor = new Label
+                                          {
+                                              Name = "BGColor",
+                                              Text = "Background Color",
+                                              Location = new Point(6, 15),
+                                              Size = new Size(92, 13)
+                                          };
                 tab.Controls.Add(backgroundColor);
 
-                var backgroundColorPanel = new Panel();
-                backgroundColorPanel.Location = new Point(104, 7);
-                backgroundColorPanel.Name = "BGColorPanel";
-                backgroundColorPanel.Size = new Size(45, 22);
-                backgroundColorPanel.BackColor = Color.FromArgb(printWorkItem.BackGroundColor);
+                var backgroundColorPanel = new Panel
+                                               {
+                                                   Location = new Point(104, 7),
+                                                   Name = "BGColorPanel",
+                                                   Size = new Size(45, 22),
+                                                   BackColor = Color.FromArgb(printWorkItem.BackGroundColor)
+                                               };
                 tab.Controls.Add(backgroundColorPanel);
 
-                var changeBGColorButton = new Button();
-                changeBGColorButton.Name = printWorkItem.Type + "_" + "BG";
-                changeBGColorButton.Text = "Change";
-                changeBGColorButton.Location = new Point(158, 6);
-                changeBGColorButton.Size = new Size(75, 23);
-                changeBGColorButton.UseVisualStyleBackColor = true;
-                changeBGColorButton.Click += pickColorButton_Click;
-                tab.Controls.Add(changeBGColorButton);
+                var changeBgColorButton = new Button
+                                              {
+                                                  Name = printWorkItem.Type + "_" + "BG",
+                                                  Text = "Change",
+                                                  Location = new Point(158, 6),
+                                                  Size = new Size(75, 23),
+                                                  UseVisualStyleBackColor = true
+                                              };
+                changeBgColorButton.Click += PickColorButtonClick;
+                tab.Controls.Add(changeBgColorButton);
 
-                var textColor = new Label();
-                textColor.Name = "TextColor";
-                textColor.Text = "Text Color";
-                textColor.Location = new Point(6, 40);
-                textColor.Size = new Size(55, 13);
+                var textColor = new Label
+                                    {
+                                        Name = "TextColor",
+                                        Text = "Text Color",
+                                        Location = new Point(6, 40),
+                                        Size = new Size(55, 13)
+                                    };
                 tab.Controls.Add(textColor);
 
-                var textColorPanel = new Panel();
-                textColorPanel.Location = new Point(66, 35);
-                textColorPanel.Name = "TextColorPanel";
-                textColorPanel.Size = new Size(45, 22);
-                textColorPanel.BackColor = Color.FromArgb(printWorkItem.TextColor);
+                var textColorPanel = new Panel
+                                         {
+                                             Location = new Point(66, 35),
+                                             Name = "TextColorPanel",
+                                             Size = new Size(45, 22),
+                                             BackColor = Color.FromArgb(printWorkItem.TextColor)
+                                         };
                 tab.Controls.Add(textColorPanel);
 
-                var changeTextColorButton = new Button();
-                changeTextColorButton.Name = printWorkItem.Type + "_" + "TEXT";
-                changeTextColorButton.Text = "Change";
-                changeTextColorButton.Location = new Point(117, 35);
-                changeTextColorButton.Size = new Size(75, 23);
-                changeTextColorButton.Click += pickColorButton_Click;
-                changeTextColorButton.UseVisualStyleBackColor = true;
+                var changeTextColorButton = new Button
+                                                {
+                                                    Name = printWorkItem.Type + "_" + "TEXT",
+                                                    Text = "Change",
+                                                    Location = new Point(117, 35),
+                                                    Size = new Size(75, 23),
+                                                    UseVisualStyleBackColor = true
+                                                };
+                changeTextColorButton.Click += PickColorButtonClick;
                 tab.Controls.Add(changeTextColorButton);
 
-                var rowRawData = new TextBox();
-                rowRawData.Location = new Point(9, 69);
-                rowRawData.Multiline = true;
-                rowRawData.Name = "RowRawData";
-                rowRawData.Text = printWorkItem.RowsRawData;
-                rowRawData.Size = new Size(392, 202);
-                rowRawData.TabIndex = 60;
-                rowRawData.WordWrap = false;
-                rowRawData.ScrollBars = ScrollBars.Both;
+                var rowRawData = new TextBox
+                                     {
+                                         Location = new Point(9, 69),
+                                         Multiline = true,
+                                         Name = printWorkItem.Type + "_RowRawData",
+                                         Text = printWorkItem.RowsRawData,
+                                         Size = new Size(392, 202),
+                                         TabIndex = 60,
+                                         WordWrap = false,
+                                         ScrollBars = ScrollBars.Both
+                                     };
+                rowRawData.TextChanged += RowRawDataTextChanged;
                 tab.Controls.Add(rowRawData);
 
                 TabControl.TabPages.Add(tab);
@@ -92,31 +105,110 @@ namespace PeterWibeck.ScrumyVSPlugin
 
             TabControl.TabPages.Add(FontsTab);
             TabControl.TabPages.Add(AdvancedTab);
+
+            int fontYPos = 30;
+            foreach (var font in settings.Fonts)
+            {
+                var fontTextBox = new TextBox
+                                      {
+                                          Location = new Point(7, fontYPos),
+                                          Name = "FontLabel_" + font.Key,
+                                          Size = new Size(100, 20),
+                                          Text = font.Key
+                                      };
+                FontsTab.Controls.Add(fontTextBox);
+
+                var fontLabel = new Label
+                                    {
+                                        AutoSize = true,
+                                        Location = new Point(123, fontYPos),
+                                        Name = "FontStyle_" + font.Key,
+                                        Size = new Size(150, 13),
+                                        Text = font.Value.Name + " " + font.Value.Size + " " + font.Value.Style
+                                    };
+                FontsTab.Controls.Add(fontLabel);
+
+                var changeFontButton = new Button
+                                           {
+                                               Location = new Point(290, fontYPos),
+                                               Name = "ChangeFontButton_" + font.Key,
+                                               Size = new Size(75, 23),
+                                               TabIndex = 4,
+                                               Text = "Change font",
+                                               UseVisualStyleBackColor = true
+                                           };
+                changeFontButton.Click += ChangeFontButtonClick;
+                FontsTab.Controls.Add(changeFontButton);
+
+                fontYPos += 30;
+            }
         }
 
-        private void pickColorButton_Click(object sender, EventArgs e)
+        private void RowRawDataTextChanged(object sender, EventArgs e)
+        {
+            var textBox = (TextBox) sender;
+
+            string[] nameSplited = textBox.Name.Split('_');
+            WorkItemPrintData workItem =
+                settings.PrintWorkItems.Where(item => item.Type.Equals(nameSplited[0])).
+                    ToArray()[0];
+            workItem.RowsRawData = textBox.Text;
+        }
+
+        private void ChangeFontButtonClick(object sender, EventArgs e)
         {
             var button = (Button) sender;
 
             string[] nameSplited = button.Name.Split('_');
-            foreach (TabPage tabPage in TabControl.TabPages.Cast<TabPage>().Where(tabPage => tabPage.Name.Equals(nameSplited[0])))
+            Font font;
+            if (settings.Fonts.TryGetValue(nameSplited[1], out font))
             {
-                foreach (Control control in tabPage.Controls)
+                fontDialog1.Font = font;
+                if (fontDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    if (control.Name.Equals("TextColorPanel") && nameSplited[1].Equals("TEXT"))
+                    foreach (Control control in FontsTab.Controls)
                     {
-                        ChangeColor((Panel) control);
-                        break;
+                        if (
+                            string.Compare(control.Name, "FontStyle_" + nameSplited[1],
+                                           StringComparison.OrdinalIgnoreCase) == 0)
+                        {
+                            control.Text = fontDialog1.Font.Name + " " + fontDialog1.Font.Size + " " +
+                                           fontDialog1.Font.Style;
+                            break;
+                        }
                     }
 
-                    if (control.Name.Equals("BGColorPanel") && nameSplited[1].Equals("BG"))
-                    {
-                        ChangeColor((Panel) control);
-                        break;
-                    }
+                    settings.Fonts[nameSplited[1]] = fontDialog1.Font;
+                }
+            }
+        }
+
+        private void PickColorButtonClick(object sender, EventArgs e)
+        {
+            var button = (Button) sender;
+
+            string[] nameSplited = button.Name.Split('_');
+            WorkItemPrintData workItem =
+                settings.PrintWorkItems.Where(item => item.Type.Equals(nameSplited[0])).
+                    ToArray()[0];
+            TabPage tabPage =
+                TabControl.TabPages.Cast<TabPage>().Where(page => page.Name.Equals(nameSplited[0])).ToArray()[0];
+
+            foreach (Control control in tabPage.Controls)
+            {
+                if (control.Name.Equals("TextColorPanel") && nameSplited[1].Equals("TEXT"))
+                {
+                    ChangeColor((Panel) control);
+                    workItem.TextColor = control.BackColor.ToArgb();
+                    break;
                 }
 
-                break;
+                if (control.Name.Equals("BGColorPanel") && nameSplited[1].Equals("BG"))
+                {
+                    ChangeColor((Panel) control);
+                    workItem.BackGroundColor = control.BackColor.ToArgb();
+                    break;
+                }
             }
         }
 
@@ -125,29 +217,40 @@ namespace PeterWibeck.ScrumyVSPlugin
             var copyOfSettings = new Settings();
             copyOfSettings.LoadSettings();
 
-            bool settingsOK = false;
+            var settingsOk = false;
             if (string.Compare(AdvanceDocument.Text, copyOfSettings.Document, StringComparison.OrdinalIgnoreCase) == 0)
             {
+                // Copy work item data to copy of settings
                 foreach (WorkItemPrintData printWorkItem in copyOfSettings.PrintWorkItems)
                 {
-                    foreach (Control control in from TabPage tabPage in TabControl.TabPages
-                                                where tabPage.Name.Equals(printWorkItem.Type)
-                                                from Control control in tabPage.Controls
-                                                select control)
+                    foreach (WorkItemPrintData setting in settings.PrintWorkItems)
                     {
-                        if (control.Name.Equals("BGColorPanel"))
+                        if (printWorkItem.Type.Equals(setting.Type))
                         {
-                            printWorkItem.BackGroundColor = control.BackColor.ToArgb();
+                            printWorkItem.BackGroundColor = setting.BackGroundColor;
+                            printWorkItem.TextColor = setting.TextColor;
+                            printWorkItem.RowsRawData = setting.RowsRawData;
                         }
+                    }
+                }
 
-                        if (control.Name.Equals("TextColorPanel"))
-                        {
-                            printWorkItem.TextColor = control.BackColor.ToArgb();
-                        }
+                // Copy font data to copy of settings
+                foreach (var font in settings.Fonts)
+                {
+                    copyOfSettings.Fonts[font.Key] = font.Value;
+                }
 
-                        if (control.Name.Equals("RowRawData"))
+                // Update the font name if changed
+                foreach (Control control in FontsTab.Controls)
+                {
+                    if (control.Name.StartsWith("FontLabel_", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string[] nameSplited = control.Name.Split('_');
+                        if (string.Compare(control.Text, nameSplited[1], StringComparison.Ordinal) != 0)
                         {
-                            printWorkItem.RowsRawData = control.Text;
+                            Font font = copyOfSettings.Fonts[nameSplited[1]];
+                            copyOfSettings.Fonts.Remove(nameSplited[1]);
+                            copyOfSettings.Fonts.Add(control.Text, font);
                         }
                     }
                 }
@@ -156,7 +259,7 @@ namespace PeterWibeck.ScrumyVSPlugin
                 {
                     copyOfSettings.SaveSettings();
                     copyOfSettings.LoadSettings();
-                    settingsOK = true;
+                    settingsOk = true;
                 }
                 catch (Exception exception)
                 {
@@ -170,16 +273,15 @@ namespace PeterWibeck.ScrumyVSPlugin
                     copyOfSettings.Document = AdvanceDocument.Text;
                     copyOfSettings.SaveSettings();
                     copyOfSettings.LoadSettings();
-                    settingsOK = true;
+                    settingsOk = true;
                 }
                 catch (Exception exception)
                 {
                     MessageBox.Show("Error:" + exception.Message, "Error when saving data", MessageBoxButtons.OK);
                 }
-                
             }
 
-            if (settingsOK)
+            if (settingsOk)
             {
                 Close();
             }
